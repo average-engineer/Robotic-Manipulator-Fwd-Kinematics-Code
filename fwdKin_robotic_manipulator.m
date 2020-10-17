a96ef93cfa4a96efa6d7625fdb3552b33ac36e4e
Ashutosh Mukherjee <ashutosh.mukherjeecpg@gmail.com>
2020-10-17 20:37:09 +0530

    Update fwdKin_robotic_manipulator.m

--- fwdKin_robotic_manipulator.m
+++ fwdKin_robotic_manipulator.m
@@ -9,9 +9,9 @@ dh_variant = input_variables(3);
 
 %deciding the DH variant to be used
 if dh_variant == 1
-    run fwdKin_HumanLeg_distalDH
+    run fwdKin_RoboticManipulator_distalDH
 elseif dh_variant == 2
-    run fwdKin_HumanLeg_proximalDH
+    run fwdKin_RoboticManipulator_proximalDH
 else
     display('Error. No recognizable DH variant to be used found!')
 end 
